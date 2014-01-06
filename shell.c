7bef6adeca8f40154090c112a34b4b40ebefbbc2
Okkervile <dahu@gmx.de>
2014-01-06 23:07:07 +0100

    added signal treatment

--- shell.c
+++ shell.c
@@ -5,15 +5,30 @@
 #include <readline/history.h>
 #include "parse.h"   //include declarations for parse-related structs
 #include <unistd.h>
-
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
-#include </development/libgphoto2-2.5.0/gphoto2/gphoto2.h>
+#include </development/libgphoto2-2.5.2/gphoto2/gphoto2.h>
 #include "samples.h"
+#include <sys/time.h>
+
+
+Camera	*canon;
+GPContext *canoncontext;
+/* Our own SIGTERM handler */
+void mysigterm()
+{
+    printf("I caught the SIGTERM signal!\n");
+    gp_camera_exit(canon, canoncontext);
+    return;
+}
+
+
+
 
 enum
 BUILTIN_COMMANDS { NO_SUCH_BUILTIN=0, EXIT,JOBS};
@@ -37,25 +52,22 @@ isBuiltInCommand(char * cmd){
 int
 main (int argc, char **argv)
 {
-    
-    
+
+	const char *version = "0.0.3";
     char * cmdLine, shell_prompt[100];
-    
+    signal(SIGTERM, mysigterm);
     int loop = 1;
     parseInfo *info; //info stores all the information returned by parser.
     struct commandType *com; //com stores command name and Arg list for one command.
     //fprintf(stderr, "Until you fix the exit command press ctrl-c to exit\n");
-    Camera	*canon;
+
   	int	i=0, retval;
-  	GPContext *canoncontext = sample_create_context();
-    
-    
+  	canoncontext= sample_create_context();
+  	//time_t  timeout = time(NULL);
+
   	gp_camera_new(&canon);
-    
-    
-    
-    
-    
+
+
     
 #ifdef UNIX
     
@@ -66,9 +78,12 @@ main (int argc, char **argv)
     fprintf(stdout, "This is the WINDOWS version\n");
 #endif
     
+
+
     while(loop){
         //insert your code to print prompt here.
-        rl_bind_key('\t', rl_complete);
+
+    	rl_bind_key('\t', rl_complete);
         snprintf(shell_prompt, sizeof(shell_prompt), "%s:%s[photoshell]$ ", getenv("USER"), getcwd(NULL, 1024));
 #ifdef UNIX
         cmdLine = readline(shell_prompt);
@@ -80,8 +95,14 @@ main (int argc, char **argv)
         // Add input to history.
         add_history(cmdLine);
         //insert your code about history and !x !-x here
-        
-        
+        time_t ctr=time(NULL);
+       /* if(ctr > timeout+100){
+        	loop=0;
+        	gp_camera_exit(canon, canoncontext);
+        	fprintf(stdout, "Timeout\n");
+        	return 0;
+        }
+        */
         //prints the info struct
         //yxcprint_info(info);
         //i=strcmp(cmdLine,"preview");
@@ -111,14 +132,17 @@ main (int argc, char **argv)
             gp_file_unref(file);
             
             i++;
+           // timeout = time(NULL);
+
         }
 
         if(!strcmp(cmdLine,"previewNAN"))
          {
-             CameraFile *file;
+
+        	 CameraFile *file;
              char output_file[32];
 
-             fprintf(stderr,"preview %d\n", i);
+             //fprintf(stderr,"preview %d\n", i);
              retval = gp_file_new(&file);
              if (retval != GP_OK) {
                  fprintf(stderr,"gp_file_new: %d\n", retval);
@@ -139,10 +163,13 @@ main (int argc, char **argv)
              gp_file_unref(file);
 
              i++;
+             //timeout = time(NULL);
+
          }
         if(!strcmp(cmdLine,"gpclose"))
         {
             gp_camera_exit(canon, canoncontext);
+            //timeout = time(NULL);
         }
         if(!strcmp(cmdLine,"gpopen"))
         {
@@ -153,21 +180,62 @@ main (int argc, char **argv)
                 //
                 //exit (1);
             }
+           // timeout = time(NULL);
+
         }
         if(!strcmp(cmdLine,"rstPicCtr"))
         {
             i=0;
+            //timeout = time(NULL);
+
         }
         if(!strcmp(cmdLine,"autofocus"))
         {
             camera_auto_focus (canon, canoncontext);
+            //timeout = time(NULL);
+
         }
         if(!strcmp(cmdLine,"exit"))
-        {
-            loop = 0;
+           {
+            loop=0;
             gp_camera_exit(canon, canoncontext);
-            
-        }
+            return 0;
+
+           }
+
+        if(!strcmp(cmdLine,"version"))
+                {
+        			fprintf(stderr,"version %s\n", version);
+                }
+
+        if(!strcmp(cmdLine,"loop"))
+    	 while(1){
+			usleep(500);
+    		 CameraFile *file;
+
+            char output_file[32];
+
+            fprintf(stderr,"preview %d\n", i);
+            retval = gp_file_new(&file);
+            if (retval != GP_OK) {
+                fprintf(stderr,"gp_file_new: %d\n", retval);
+                exit(1);
+            }
+
+            retval = gp_camera_capture_preview(canon, file, canoncontext);
+            if (retval != GP_OK) {
+                fprintf(stderr,"gp_camera_capture_preview(%d): %d\n", i, retval);
+                exit(1);
+            }
+            sprintf(output_file, "snapshot.jpg", i);
+            retval = gp_file_save(file, output_file);
+            if (retval != GP_OK) {
+                fprintf(stderr,"gp_camera_capture_preview(%d): %d\n", i, retval);
+                exit(1);
+            }
+            gp_file_unref(file);
+    		}
+
         // free_info(info);
         free(cmdLine);
     }/* while(1) */
